0595e1cddc0b80bab59adf89730421def601cabb
Usmon Kurbanbaev <usmonkurbanbaev1@gmail.com>
2023-02-08 16:58:43 +0500

    for OOP task

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,45 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(){
+    }
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int paws) {
+        this.numberOfPaws = paws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        return "This animal is mostly "+color+". It has "+numberOfPaws+" "+(numberOfPaws==1?"paw":"paws")+ " and "+
+                (hasFur?"a":"no")+" fur.";
+    }
 }
